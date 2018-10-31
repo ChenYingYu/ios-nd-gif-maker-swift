d7260ccb5769163c0e2357db3e7273435f8b205e
Alan Chen <Alan@vencers-13-macbook-air.local>
2018-10-31 17:57:13 +0800

    Add a gif to gifImageView.

--- GifMaker_Swift_Template/WelcomeViewController.swift
+++ GifMaker_Swift_Template/WelcomeViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    @IBOutlet weak var gifImageView: UIImageView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        let proofOfConceptGif = UIImage.gif(name: "hotlineBling")
+        gifImageView.image = proofOfConceptGif
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
