f6b92d86a193c19f0a473822776e10102c58b1fe
Alan Chen <Alan@vencers-13-macbook-air.local>
2018-11-01 10:58:15 +0800

    Gif Model

---
+++ GifMaker_Swift_Template/Gif.swift
@@ -0,0 +1,26 @@
+//
+//  Gif.swift
+//  GifMaker_Swift_Template
+//
+//  Created by AlanChen on 2018/11/1.
+//  Copyright © 2018年 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Gif {
+    let url: NSURL
+    let caption: String?
+    let gifImage: UIImage?
+    let videoURL: NSURL
+    var gifData: NSData?
+    
+    init(url:NSURL, videoURL:NSURL, caption: String?) {
+        self.url = url
+        self.videoURL = videoURL
+        self.caption = caption
+        self.gifImage = UIImage.gif(url: url.absoluteString ?? "")
+        self.gifData = nil
+    }
+}
