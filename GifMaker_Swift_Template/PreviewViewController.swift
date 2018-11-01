08c6aa7c10bf26974b3c00d2a5683b5afcd56c2f
Alan Chen <Alan@vencers-13-macbook-air.local>
2018-11-01 15:29:40 +0800

    share Gif

--- GifMaker_Swift_Template/PreviewViewController.swift
+++ GifMaker_Swift_Template/PreviewViewController.swift
@@ -13,6 +13,25 @@ class PreviewViewController: UIViewController {
     @IBOutlet weak var previewImageView: UIImageView!
     var gif: Gif?
     
+    @IBAction func shareGif(_ sender: UIButton) {
+        guard let gifURL = self.gif?.url else {
+            return
+        }
+        guard let animatedGif = NSData(contentsOf: gifURL as URL) else {
+            return
+        }
+        let itemsToShare: [Any] = [animatedGif]
+        let shareController = UIActivityViewController(activityItems: itemsToShare, applicationActivities: nil)
+        
+        shareController.completionWithItemsHandler = {(activity, completed, returnItems, error) in
+            if (completed) {
+                self.navigationController?.popToRootViewController(animated: true)
+            }
+        }
+        
+        self.present(shareController, animated: true, completion: nil)
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         previewImageView.image = gif?.gifImage
     }
