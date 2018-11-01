e1df85bd9bac42296bbd50a9611a8c332cf28187
Alan Chen <Alan@vencers-13-macbook-air.local>
2018-11-01 11:59:33 +0800

    Observe and respond to keyboard notifications.

--- GifMaker_Swift_Template/GifEditorViewController.swift
+++ GifMaker_Swift_Template/GifEditorViewController.swift
@@ -16,12 +16,17 @@ class GifEditorViewController: UIViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         gifImageView.image = gif?.gifImage
+        subscribeToKeyboardNotifications()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        unsubscribeFromKeyboardNotifications()
     }
 }
 
@@ -34,5 +39,37 @@ extension GifEditorViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+}
+
+extension GifEditorViewController {
+    // MARK: Observe and respond to keyboard notifications
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: Notification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: Notification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: Notification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: Notification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func keyboardWillShow(notification: Notification) {
+        if (self.view.frame.origin.y >= 0) {
+            self.view.frame.origin.y -= self.getKeyboardHeight(notification: notification)
+        }
+    }
+    
+    func keyboardWillHide(notification: Notification) {
+        if (self.view.frame.origin.y < 0) {
+            self.view.frame.origin.y += self.getKeyboardHeight(notification: notification)
+        }
+    }
+    
+    func getKeyboardHeight(notification: Notification) -> CGFloat {
+        if let userInfo = notification.userInfo, let keyboardFrameEnd = userInfo[UIKeyboardFrameEndUserInfoKey] as? CGRect {
+            return keyboardFrameEnd.size.height
+        }
+        return 0.0
     }
 }
